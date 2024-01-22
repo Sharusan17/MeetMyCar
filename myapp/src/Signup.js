d5f3e03846defba9f11cf8e95325c3b468a5c3c2
Sharusan17 <shariharan17@icloud.com>
2024-01-22 21:50:21 +0000

    added fetchUserData feature
    added extra user details in signup

--- myapp/src/Signup.js
+++ myapp/src/Signup.js
@@ -5,36 +5,93 @@ import {Link, useNavigate} from 'react-router-dom'
 
 import './Signup_css.css'
 
-
-
 const Signup = () => {
 
+    const firstNameRef = useRef()
+    const lastNameRef = useRef()
+    const userNameRef = useRef()
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+
     const {signup} = useAuth()
+
     const [error, setError] = useState('')
     const [message] = useState('')
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
 
-
     async function handleSubmit(e){
         e.preventDefault()
+        
+        if (firstNameRef.current.value.length < 2){
+             return setError("Firstname Minimum Length (2)")
+        }
+
+        if (lastNameRef.current.value.length < 2){
+            return setError("Lastname Minimum Length (2)")
+        }
+
+        if (userNameRef.current.value.length < 2){
+            return setError("Username Minimum Length (2)")
+        }
+
+        if (userNameRef.current.value.length > 20){
+            return setError("Username Maximum Length (20)")
+        }
 
         if (passwordRef.current.value !== passwordConfirmRef.current.value){
             return setError("Password Not The Same")
         }
+
         if (passwordRef.current.value.length < 6 && passwordConfirmRef.current.value.length < 6 ){
             return setError("Password Really Short")
         }
+
+        const userData = {
+            firstname: firstNameRef.current.value,
+            lastname: lastNameRef.current.value,
+            username: userNameRef.current.value,
+            email: emailRef.current.value,
+        }
+
         try{
             setError('')
-            await signup(emailRef.current.value, passwordRef.current.value)
-            navigate("/registercar")
-        }catch{
-            setError("Failed To Create Account")
+            const {user} = await signup(emailRef.current.value, passwordRef.current.value)
+
+            const firebaseUID = user.uid;
+
+            const response = await fetch('http://localhost:3001/users/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ ...userData, user_fbId: firebaseUID }),
+            });
+
+            if (response.ok){
+                const data = await response.json()
+                console.log("Created User Account Successful")
+                navigate("/registervehicle")
+                return data
+            } else{
+                const errorData = await response.json()
+                if (errorData.message === 'Username Already Taken'){
+                    return setError("Username Already in Use")
+                }else{
+                    throw new Error(errorData.message)
+                }
+            }
+        }catch (error){
+            console.error("Error Signing Up:", error)
+            if (error.code === 'auth/email-already-in-use'){
+                setError("Email Already In Use")
+            } else if (error.code === 'auth/invalid-email'){
+                setError("Invalid Email Address")
+            }else{
+                setError("Failed To Create Account")
+            }
         }
         setLoading(false) 
     }
@@ -42,58 +99,58 @@ const Signup = () => {
 
   return (
       <>
-            <div className="Card">
-                
-                <header>
-                    <h1 id="sign_text">
-                        Sign Up
-                        <p id="slogan_text">Join Our Family</p>
-                    </h1>
-                    <div id="red-corner"></div>
-                </header>
+        <div className="Card">
+            
+            <header>
+                <h1 id="sign_text">
+                    Sign Up
+                    <p id="slogan_text">Join Our Family</p>
+                </h1>
+                <div id="red-corner"></div>
+            </header>
 
-                <form onSubmit={handleSubmit} className='signUpForm'>
+            <form onSubmit={handleSubmit} className='signUpForm'>
 
-                    <div className='name_row'>
-                        <Form.Group id="firstName">
-                            <Form.Control id="name_input" type="text" placeholder='First Name' ref={emailRef} required />
-                        </Form.Group>
-
-                        <Form.Group id="lastName" >
-                            <Form.Control id="name_input" type="text" placeholder='Last Name' ref={emailRef} required />
-                        </Form.Group>
-
-                    </div>
-                    
-                        
-
-                    <Form.Group id="email">
-                        <Form.Control type="email" placeholder='Email'ref={emailRef} required />
-                    </Form.Group>            
-                    
-                    <Form.Group id="password">
-                        <Form.Control type="password" placeholder='Password'ref={passwordRef} required />
+                <div className='name_row'>
+                    <Form.Group id="firstName">
+                        <Form.Control id="name_input" type="text" placeholder='First Name' ref={firstNameRef} required />
                     </Form.Group>
 
-                    <Form.Group id="passwordConfirmation">
-                        <Form.Control type="password" placeholder='Password Confirmation'ref={passwordConfirmRef} required />
+                    <Form.Group id="lastName" >
+                        <Form.Control id="name_input" type="text" placeholder='Last Name' ref={lastNameRef} required />
                     </Form.Group>
-
-                    <p className="w-100 text-center mt-3 mb-1" id="error_Msg">{error}</p>
-
-                    <button disabled={loading} id="button" className="w-100 mt-2" type="submit">Sign Up</button>
-                        
-
-                    {message}
-                </form>
-
-                <img src="https://i.ibb.co/FsSc9k1/Screenshot-2023-11-07-at-18-34-47-fotor-bg-remover-20231107185715.png" alt="Porsche Car"/>
-                
-                <div id="loginpage" className="w-100 text-center mt-2">
-                    Already have an account? <Link to="/login" id='loginlink'> Login Here</Link>
                 </div>
 
+                <Form.Group id="userName">
+                    <Form.Control type="text" placeholder='Username'ref={userNameRef} required />
+                </Form.Group>     
+
+                <Form.Group id="email">
+                    <Form.Control type="email" placeholder='Email'ref={emailRef} required />
+                </Form.Group>            
+                
+                <Form.Group id="password">
+                    <Form.Control type="password" placeholder='Password'ref={passwordRef} required />
+                </Form.Group>
+
+                <Form.Group id="passwordConfirmation">
+                    <Form.Control type="password" placeholder='Password Confirmation'ref={passwordConfirmRef} required />
+                </Form.Group>
+
+                <p className="w-100 text-center mt-3 mb-1" id="error_Msg">{error}</p>
+
+                <button disabled={loading} id="button" className="w-100 mt-2" type="submit">Sign Up</button>
+                    
+                {message}
+            </form>
+
+            <img src="https://i.ibb.co/FsSc9k1/Screenshot-2023-11-07-at-18-34-47-fotor-bg-remover-20231107185715.png" alt="Porsche Car"/>
+            
+            <div id="loginpage" className="w-100 text-center mt-2">
+                Already have an account? <Link to="/login" id='loginlink'> Login Here</Link>
             </div>
+
+        </div>
         
       </>
     
--- myapp/src/UpdateProfile.js
+++ myapp/src/UpdateProfile.js
@@ -1,18 +1,69 @@
-import React, {useRef, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {Form, Button} from 'react-bootstrap'
 import { useAuth } from './AuthContext'
 import {Link, useNavigate} from 'react-router-dom'
 
 const UpdateProfile = () => {
 
+    const usernameRef = useRef()
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const profilePictureRef = useRef()
+    const vehicleRef = useRef()
+
+    const [firstname, setfirstName] = useState('')
+    const [lastname, setlastName] = useState('')
+    const [username, setuserName] = useState('')
+    const [profilePicture, setprofilePicture] = useState('')
+    const [vehicles, setVehicles] = useState([])
+
     const {currentUser, updateEmail, updatePassword} = useAuth()
+
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
+    useEffect(() => {
+        async function fetchUserData(){
+            try{
+                setError('')
+                const firebaseUID = currentUser.uid;
+                // console.log(firebaseUID)
+
+                const response = await fetch(`http://localhost:3001/users/register?userfb=${encodeURIComponent(firebaseUID)}`, {
+                    method: 'GET',
+                    headers: {
+                        'accept': 'application/json',
+                    },
+                });
+
+                if (response.ok){
+                    const data = await response.json()
+
+                    setfirstName(data.userData.firstname)
+                    setlastName(data.userData.lastname)
+                    setuserName(data.userData.username)
+                    setprofilePicture(data.userData.profilePicture)
+                    setVehicles(data.userData.vehicles)
+
+                    // console.log(`User: FirstName ${firstname} LastName: ${lastname}  UserName: ${username}
+                    //              Profile Picture: ${profilePicture} Vehicles: ${vehicles}`)   
+
+                    console.log("Fetched User Details")
+                    return data
+                } else{
+                    const errorData = await response.json()
+                    setError("Failed To Fetch User Data")
+                    throw new Error(errorData.message)
+                }
+            }catch (error){
+                console.error("Error Fetching User Data:", error)
+                setError("Failed To Fetch User Data")
+            }
+        }
+        fetchUserData();
+    }, [currentUser.uid]); 
 
 
     async function handleSubmit(e){
@@ -38,19 +89,29 @@ const UpdateProfile = () => {
 
         Promise.all(toupdate).then(() => {
             navigate('/')
-        }).catch( () => {
-            setError("Failed to Update Account")
+        }).catch( (error) => {
+            console.log("Failed to Update Account" , error)
+            if (error.code === 'auth/operation-not-allowed'){
+                setError("Verify Current Email Address")
+            }else{
+                setError("Failed to Update Account")
+            }
         }).finally( () => {
             setLoading(false)
         })
     }
 
-
   return (
       <>
             <body>
-                <h1 className="text-center mb-4">Update Profile</h1>
+                <h3 className="text mb-2">{firstname} {lastname}</h3>
+                <h4 className="text mb-4">Update Profile</h4>
                 <form onSubmit={handleSubmit}>
+
+                <Form.Group id="username">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" ref={usernameRef}  defaultValue={username} disabled/>
+                    </Form.Group>   
                     
                     <Form.Group id="email">
                         <Form.Label>Email</Form.Label>
@@ -67,6 +128,16 @@ const UpdateProfile = () => {
                         <Form.Control type="password" ref={passwordConfirmRef} placeholder='Leave Blank to keep the same'  />
                     </Form.Group>
 
+                    <Form.Group id="profilePicture">
+                        <Form.Label>Profile Picture</Form.Label>
+                        <Form.Control type="" ref={profilePictureRef} defaultValue={profilePicture} placeholder=''  />
+                    </Form.Group>
+
+                    <Form.Group id="vehicles">
+                        <Form.Label>Look At Your Vehicles</Form.Label>
+                        <Form.Control type="array" ref={vehicleRef} defaultValue={vehicles} placeholder=''  />
+                    </Form.Group>
+
                     <p>{error}</p>
 
                     <Button disabled={loading} className="w-100 mt-2" type="submit">Update</Button>
@@ -79,7 +150,6 @@ const UpdateProfile = () => {
             <Link to="/">  Go Back Home</Link>
         </div>
       </>
-    
   )
 }
 
