aee2b8a3e002d37f8b835dddbda1e485ce66299b
Sharusan17 <shariharan17@icloud.com>
2024-03-08 23:19:22 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,27 +11,37 @@ const postRoutes = require('./routes/postRoutes');
 const vehicleRoutes = require('./routes/vehicleRoutes');
 
 dotenv.config();
-connectDB();
 
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Backend Server")
+    res.send("Backend Server");
 })
 
-app.use((req, res, next) => {
-    req.s3 = connectAWS.s3;
-    req.s3_BUCKET = connectAWS.S3_BUCKET;
-    req.uploadAWS = connectAWS.uploadAWS;
-    next();
-})
+const connection = async () => {
+    try{
+        await connectDB();
 
-app.use('/vehicleAPI', vehicleAPIRoutes);
-app.use('/users', userRoutes);
-app.use('/posts', postRoutes);
-app.use('/vehicles', vehicleRoutes);
+        app.use((req, res, next) => {
+            req.s3 = connectAWS.s3;
+            req.s3_BUCKET = connectAWS.S3_BUCKET;
+            req.uploadAWS = connectAWS.uploadAWS;
+            next();
+        })
+        
+        app.use('/vehicleAPI', vehicleAPIRoutes);
+        app.use('/users', userRoutes);
+        app.use('/posts', postRoutes);
+        app.use('/vehicles', vehicleRoutes);
+        
+        
+        const PORT = 3001;
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    } catch (error){
+        console.error("Cannot Connect To Database:", error);
+    }
+}
 
+connection();
 
-const PORT = 3001;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
