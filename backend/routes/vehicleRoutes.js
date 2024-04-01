9043c9a5fd202aae716100b24060022e74ce5181
Sharusan17 <shariharan17@icloud.com>
2024-04-01 15:24:17 +0100

    added comments to vehicleRoutes

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -1,3 +1,4 @@
+// import modules
 const express = require('express');
 const Vehicle = require('../model/vehicles');
 const router = express.Router();
@@ -6,20 +7,25 @@ const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({storage: storage});
 
+// parse as JSON
 router.use(express.json());
 
 //Get Vehicle Data
 router.get('/', async (req, res) => {
     console.log(req.body);
     try{
+        // parameters from query string (vehicleId)
         const vehicleId = req.query.vehicleId;
 
+        // return a 400 Bad response, if no vehicle id found
         if(!vehicleId){
             return res.status(400).json({message: "Vehicle Not Found."})
         }
-    
+
+        // Find vehicle data by vehicleId and populate the user
         vehicleData = await Vehicle.findOne({_id: vehicleId}).populate('user', 'username');
 
+        // return a 400 Bad response, if no vehicle data found
         if(!vehicleData){
             return res.status(404).json({message: "Vehicle Details Not Found."});
         }
@@ -36,9 +42,10 @@ router.get('/', async (req, res) => {
 router.post('/add', upload.single('image'), async (req, res) => {
     console.log(req.body);
     try{       
+        // Create a new vehicle with body data
         const newVehicle = new Vehicle(req.body);
 
-        //Download The VehicleImg Into AWS
+        // Upload The VehicleImg Into AWS and store the image
         if (req.body.image){
             const imageResponse= await fetch(req.body.image);
             const imageData = await imageResponse.arrayBuffer();
@@ -48,26 +55,27 @@ router.post('/add', upload.single('image'), async (req, res) => {
         }
         
 
-        //Parses The VehicleInfo Into Nested Objects
+        // Parses The VehicleInfo Into Nested Objects
         if (req.body.vehicleInfo){
             newVehicle.vehicleInfo = JSON.parse(req.body.vehicleInfo)
         }
 
-        //Parses The VehicleValue Into Nested Objects
+        // Parses The VehicleValue Into Nested Objects
         if (req.body.vehicleValue){
             newVehicle.vehicleValue = JSON.parse(req.body.vehicleValue)
         }
 
-        //Parses The VehicleMOT Into Nested Objects
+        // Parses The VehicleMOT Into Nested Objects
         if (req.body.vehicleMOT){
             newVehicle.vehicleMOT = JSON.parse(req.body.vehicleMOT)
         }
 
-        //Parses The VehicleHistory Into Nested Objects
+        // Parses The VehicleHistory Into Nested Objects
         if (req.body.vehicleHistory){
             newVehicle.vehicleHistory = JSON.parse(req.body.vehicleHistory)
         }
 
+        // Save the vehicle to database
         await newVehicle.save();
 
         res.status(201).json({message: "Vehicle Added Successfully", newVehicle});
@@ -81,38 +89,49 @@ router.post('/add', upload.single('image'), async (req, res) => {
 //UPDATE Vehicle Data
 router.put('/edit', upload.single('image'), async (req, res) => {
     console.log(req.body)
-    try{       
+    try{     
+        // parameters from query string (vehicleId)  
         const vehicleId = req.query.vehicleId;
 
+        // return a 400 Bad response, if no vehicle id
         if(!vehicleId){
             return res.status(400).json({message: "Vehicle Not Found."})
         }
+        // Find a vehicle with vehicleId and returns updated data
         const vehicleUpdate = await Vehicle.findById(vehicleId, {new: true});
 
+        // return a 400 Bad response, if no vehicle data found
         if(!vehicleUpdate){
             return res.status(404).json({message: "Vehicle Not Able To Update."});
         }
 
+        // updates vrn, if vrn in body found
         if(req.body.vrn){
             vehicleUpdate.vrn = req.body.vrn;
         }
+        // updates image, if image in body found
         if (req.file){
             const uploadResult = await req.uploadAWS(req.file);
             vehicleUpdate.image = uploadResult.Location;
         }
+        // updates vehicleInfo, if vehicleInfo in body found
         if(req.body.vehicleInfo){
             vehicleUpdate.vehicleInfo = req.body.vehicleInfo;
         }
+        // updates vehicleValue, if vehicleValue in body found
         if (req.body.vehicleValue){
             newVehicle.vehicleValue = JSON.parse(req.body.vehicleValue)
         }
+        // updates vehicleMOT, if vehicleMOT in body found
         if (req.body.vehicleMOT){
             newVehicle.vehicleMOT = JSON.parse(req.body.vehicleMOT)
         }
+        // updates vehicleHistory, if vehicleHistory in body found
         if (req.body.vehicleHistory){
             newVehicle.vehicleHistory = JSON.parse(req.body.vehicleHistory)
         }
 
+        // Save the vehicle's changes to database
         await vehicleUpdate.save();
   
         res.status(200).json({message: "Vehicle Updated", vehicle: vehicleUpdate});
@@ -125,15 +144,18 @@ router.put('/edit', upload.single('image'), async (req, res) => {
 
 //DELETE Vehicle Data
 router.delete('/delete', async (req, res) => {
-    try{       
+    try{      
+        // parameters from query string (vehicleId) 
         const vehicleId = req.query.vehicleId;
 
+        // return a 400 Bad response, if vehicle not found
         if(!vehicleId){
             return res.status(400).json({message: "Vehicle Not Found."})
         }
-
+        // Find and delete vehicle based on vehicleId
         const vehicleDeleted = await Vehicle.findByIdAndDelete( vehicleId);
   
+        // returns a 404 Not Found response, if vehicle not found
         if(!vehicleDeleted){
             return res.status(404).json({message: "Vehicle Not Able To Remove."});
         }
@@ -146,4 +168,5 @@ router.delete('/delete', async (req, res) => {
     }
 });
 
+// export the Vehicle Route
 module.exports = router;
