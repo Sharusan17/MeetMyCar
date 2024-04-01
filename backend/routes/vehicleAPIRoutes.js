e20371572f94e1994352e34370753a2be7b18bd7
Sharusan17 <shariharan17@icloud.com>
2024-04-01 15:32:04 +0100

    added comments to vehicleAPIRoutes

--- backend/routes/vehicleAPIRoutes.js
+++ backend/routes/vehicleAPIRoutes.js
@@ -1,11 +1,13 @@
+// import modules
 const express = require('express');
 const router = express.Router();
 
 //VehicleAPI List
 router.get('/list', async (req, res) => {
-
+    // import node-fetch
     const fetch = (await import('node-fetch')).default;
     try {
+        // filter vehicle with Make and Model
         const where = encodeURIComponent(JSON.stringify({
             "Make": {
               "$exists": true
@@ -15,9 +17,11 @@ router.get('/list', async (req, res) => {
             }
         }));
 
+        // fetch Parse App ID and REST API Key from env
         const APP_ID = process.env.PARSE_APP_ID;
         const REST_API_KEY = process.env.PARSE_REST_API_KEY;
 
+        // fetch vehicle list data from external API
         const apiResponse = await fetch(`https://parseapi.back4app.com/classes/Carmodels_Car_Model_List?limit=9899&order=Make,Model&keys=Make,Model&where=${where}`,
             {
                 headers:{
@@ -27,9 +31,10 @@ router.get('/list', async (req, res) => {
                 }
             }
         );
-
+        
         const responseBody = await apiResponse.text();
 
+        // successful response, parse response as JSON
         if (apiResponse.ok) {
             const data = JSON.parse(responseBody);
             res.json(data);
@@ -50,17 +55,21 @@ router.get('/search', async (req, res) => {
 
     const fetch = (await import('node-fetch')).default;
     try {
+        // fetch CAR_LIVE_API and CAR_TEST_API from env
         const LIVE_APP_ID = process.env.CAR_LIVE_API;
         const TEST_APP_ID = process.env.CAR_TEST_API;
 
+        // fetch vrn from query
         const VehicleReg = req.query.vrn
 
+        // return 400 Bad response, if vrn is empty
         if(!VehicleReg){
             return res.status(400).json({message: "Vehicle Registration Number Required."})
         }
 
         // console.log(VehicleReg);
 
+        // fetch vehicle data from external API with vrn and API key
         const apiResponse = await fetch(`https://api.checkcardetails.co.uk/vehicledata/ukvehicledata?apikey=${TEST_APP_ID}&vrm=${VehicleReg}`
             ,{
                 method:'GET',
@@ -72,6 +81,7 @@ router.get('/search', async (req, res) => {
 
         const datainfo = await apiResponse.json();
 
+        // fetch vehicle image from external API with vrn and API key
         const apiImageResponse = await fetch(`https://api.checkcardetails.co.uk/vehicledata/vehicleimage?apikey=${TEST_APP_ID}&vrm=${VehicleReg}`
             ,{
                 method:'GET',
@@ -83,6 +93,7 @@ router.get('/search', async (req, res) => {
 
         const dataImg = await apiImageResponse.json();
 
+        // fetch vehicle value from external API with vrn and API key
         const apiValueResponse = await fetch(`https://api.checkcardetails.co.uk/vehicledata/vehiclevaluation?apikey=${TEST_APP_ID}&vrm=${VehicleReg}`
             ,{
                 method:'GET',
@@ -94,6 +105,7 @@ router.get('/search', async (req, res) => {
 
         const dataValue = await apiValueResponse.json();
 
+        // fetch vehicle MOT from external API with vrn and API key
         const apiMOTResponse = await fetch(`https://api.checkcardetails.co.uk/vehicledata/mot?apikey=${TEST_APP_ID}&vrm=${VehicleReg}`
             ,{
                 method:'GET',
@@ -105,17 +117,19 @@ router.get('/search', async (req, res) => {
 
         const dataMOT = await apiMOTResponse.json();
 
+        // fetch vehicle history from external API with vrn and API key
         const apiHistoryResponse = await fetch(`https://api.checkcardetails.co.uk/vehicledata/vehicleregistration?apikey=${TEST_APP_ID}&vrm=${VehicleReg}`
-        ,{
-            method:'GET',
-            headers:{
-                'accept': 'application/json',
-            },
-        }
-    );
+            ,{
+                method:'GET',
+                headers:{
+                    'accept': 'application/json',
+                },
+            }
+        );
 
-    const dataHistory = await apiHistoryResponse.json();
+        const dataHistory = await apiHistoryResponse.json();
 
+        // successful responses, return all data as JSON
         if (apiResponse.ok && apiImageResponse.ok && apiValueResponse.ok && apiMOTResponse.ok && apiHistoryResponse.ok) {
             const responseData = {
                 datainfo,
@@ -134,4 +148,5 @@ router.get('/search', async (req, res) => {
     }
 });
 
+// export the Vehicle API Route
 module.exports = router;
